8ee2ad8e8966e86a2863798976ba8dbac33d3d30
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-24 14:56:44 -0700

    add global stylings

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,18 +2,17 @@ import products from "./products.js";
 
 const Home = () => {
     return (
-        <div>
-            <h1>E-Commerce Store</h1>
-             <div>
-            <h1>Products</h1>
-                <div className="grid grid-cols-3">
-                    {products.map((product) => (
-                    <div key={product.id} className="border p-4 w-fit ">
-                        <h3>{product.name}</h3>
-                        <p>${product.price}</p>
-                    </div>
-                    ))}
+        <div className="flex flex-col items-center space-y-12">
+            <h1 className="text-2xl tracking-widest pt-12">yarn<span className="italic">sara</span></h1>
+            <p>100% upcycled crocheted yoga accessories</p>   
+            <h3>Products</h3>
+            <div>
+                {products.map((product) => (
+                <div key={product.id} className="border p-4 m-2">
+                    <h3>{product.name}</h3>
+                    <p>${product.price}</p>
                 </div>
+                ))}
             </div>
         </div>
     )
