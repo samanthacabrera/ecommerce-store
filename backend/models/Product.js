2980e266dee50f9e01599533b5e0a38495924e9d
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-28 10:17:50 -0700

    update product model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    inventory: Object,
-    image: String,
-    material: String,
-    saved: Number,
-    description: String
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    inventory: {
+        light: { type: Number, default: 0 },
+        dark: { type: Number, default: 0 },
+        multi: { type: Number, default: 0 }
+    },
+    image: { type: String, required: true },
+    material: { type: String, required: true },
+    saved: { type: Number, default: 0 },
+    description: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Product", productSchema);
